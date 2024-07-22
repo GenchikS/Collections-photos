d4a951eeba16b9408aafa96b28a709c8b42bbf63
GenchikS <106490249+GenchikS@users.noreply.github.com>
2024-07-22 17:54:21 +0000

    Deploying to gh-pages from @ GenchikS/Collections-photos@d1089fbc0da55985cb9b0587c2713f0892cb1682 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,14 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const n="44770113-cb4279c01992ac20f8c79d080";function c(t){return fetch(`https://pixabay.com/api/?key=${n}&q=${t}&image_type=photo&orientation=horizontal&safesearch=true&per_page=10`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})}document.querySelector(".photo-gallery");function u(t){console.log("arrey",t)}const i=document.querySelector(".form"),l=document.querySelector(".photo-gallery");i.addEventListener("submit",a);let r="";function a(t){t.preventDefault();const e=t.target;if(r=e.elements.input.value.trim(),console.log("input",r),r===""){console.log("Введіть пошук");return}else c(r).then(o=>{if(Number(o.hits.length)===0)return console.log("Фото не знайдені");l.innerHTML="",u(o.hits)}).catch(o=>console.log("error",o));e.reset()}
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const i="44770113-cb4279c01992ac20f8c79d080";function c(e){return fetch(`https://pixabay.com/api/?key=${i}&q=${e}&image_type=photo&orientation=horizontal&safesearch=true&per_page=9`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}const s=document.querySelector(".photo-gallery");function a(e){console.log("arrey",e);const t=e.map(({webformatURL:o,largeImageURL:r,tags:n})=>`
+        <li class="container-photo">
+        <a href="${r}"></a>
+        <img src="${o}" alt="${n}" class="img-photo">
+            <ul class="list-text">
+                <li><p class=""></p></li>
+                <li><p class=""></p></li>
+                <li><p class=""></p></li>
+                <li><p class=""></p></li>
+            </ul>
+        </img>
+      </li>
+    `).join("");s.insertAdjacentHTML("beforeend",t)}const p=document.querySelector(".form"),u=document.querySelector(".photo-gallery");p.addEventListener("submit",m);let l="";function m(e){e.preventDefault();const t=e.target;if(l=t.elements.input.value.trim(),console.log("input",l),l===""){console.log("Введіть пошук");return}else c(l).then(o=>{if(Number(o.hits.length)===0)return console.log("Фото не знайдені");u.innerHTML="",a(o.hits)}).catch(o=>console.log("error",o));t.reset()}
 //# sourceMappingURL=commonHelpers2.js.map
