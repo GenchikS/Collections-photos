d1089fbc0da55985cb9b0587c2713f0892cb1682
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-07-22 20:53:56 +0300

    v.1.1.5(render end)

--- src/js/render_getMarcup.js
+++ src/js/render_getMarcup.js
@@ -2,4 +2,20 @@ const photoGallery = document.querySelector(".photo-gallery");
 
 export function getMarcup(arrey) {
     console.log("arrey", arrey)
+
+    const marcup = arrey.map(({webformatURL, largeImageURL, tags}) => `
+        <li class="container-photo">
+        <a href="${largeImageURL}"></a>
+        <img src="${webformatURL}" alt="${tags}" class="img-photo">
+            <ul class="list-text">
+                <li><p class=""></p></li>
+                <li><p class=""></p></li>
+                <li><p class=""></p></li>
+                <li><p class=""></p></li>
+            </ul>
+        </img>
+      </li>
+    `).join(``)
+    
+    photoGallery.insertAdjacentHTML(`beforeend`, marcup)
 }
