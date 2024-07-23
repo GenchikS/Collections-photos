f556801c2c9b5cb65b7481c59a53a2dcfb2b9b00
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-07-23 15:24:10 +0300

    v.1.1.6(corre)

--- src/js/pixabay_api_page_1.js
+++ src/js/pixabay_api_page_1.js
@@ -1,7 +1,7 @@
 const KEY_API = `44770113-cb4279c01992ac20f8c79d080`;
 
 export function getSoursePhoto(inputText) {
-    return fetch(`https://pixabay.com/api/?key=${KEY_API}&q=${inputText}&image_type=photo&orientation=horizontal&safesearch=true&per_page=9`)
+    return fetch(`https://pixabay.com/api/?key=${KEY_API}&q=${inputText}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12`)
         .then((response) => {
             if (!response.ok) {
                 throw new Error(response.status);
--- src/js/render_getMarcup.js
+++ src/js/render_getMarcup.js
@@ -3,15 +3,15 @@ const photoGallery = document.querySelector(".photo-gallery");
 export function getMarcup(arrey) {
     console.log("arrey", arrey)
 
-    const marcup = arrey.map(({webformatURL, largeImageURL, tags}) => `
+    const marcup = arrey.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
         <li class="container-photo">
         <a href="${largeImageURL}"></a>
         <img src="${webformatURL}" alt="${tags}" class="img-photo">
-            <ul class="list-text">
-                <li><p class=""></p></li>
-                <li><p class=""></p></li>
-                <li><p class=""></p></li>
-                <li><p class=""></p></li>
+            <ul class="list-text relative">
+                <li class="list-title"><p class="list-text-img">likes: ${likes}</p></li>
+                <li class="list-title"><p class="list-text-img">views: ${views}</p></li>
+                <li class="list-title"><p class="list-text-img">comments: ${comments}</p></li>
+                <li class="list-title"><p class="list-text-img">downloads: ${downloads}</p></li>
             </ul>
         </img>
       </li>
