1cb4d30b2899448bf228f922a521a9f3c3049284
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-07-23 19:13:50 +0300

    v.1.1.7

--- src/js/page-1.js
+++ src/js/page-1.js
@@ -2,7 +2,8 @@ import { getSoursePhoto } from "./pixabay_api_page_1";
 import { getMarcup } from "./render_getMarcup";
 
 const inputForm = document.querySelector(".form");
-const photoGallery = document.querySelector(".photo-gallery")
+const photoGallery = document.querySelector(".photo-gallery");
+const buttonNext = document.querySelector(".button-next")
 
 
 inputForm.addEventListener("submit", inputFormSubmit)
@@ -12,8 +13,8 @@ let inputText = "";
 function inputFormSubmit(event) {
     event.preventDefault();
     const form = event.target;
-    inputText = (form.elements.input.value).trim(); 
-    console.log("input", inputText);
+    inputText = (form.elements.input.value).trim().toLowerCase(); 
+    // console.log("input", inputText);
     if (inputText === "") {
         console.log("Введіть пошук")
         return
@@ -22,13 +23,28 @@ function inputFormSubmit(event) {
             .then((data) => {
                 if (Number(data.hits.length) === 0) {
                     return console.log("Фото не знайдені")
-                }
-                photoGallery.innerHTML = "";
-                getMarcup(data.hits);
+                }else if( 12 >  Number(data.hits.length)){
+                        photoGallery.innerHTML = "";
+                        getMarcup(data.hits);
+                        buttonNextPhotoAdd();
+                }else{
+                        photoGallery.innerHTML = "";
+                        getMarcup(data.hits);
+                        buttonNextPhotoRemove();
+                    }
                 })
-            .catch((error) => console.log("error", error))
+            .catch((error) => console.log("error server. Спробуйте ще раз", error))
         }
     form.reset();
+}
+
+
+function buttonNextPhotoRemove(){
+    buttonNext.classList.remove("hidden")
+}
+
+function buttonNextPhotoAdd(){
+    buttonNext.classList.add("hidden")
 }
 
 
--- src/js/render_getMarcup.js
+++ src/js/render_getMarcup.js
@@ -1,17 +1,16 @@
 const photoGallery = document.querySelector(".photo-gallery");
 
 export function getMarcup(arrey) {
-    console.log("arrey", arrey)
-
+    // console.log("arrey", arrey)
     const marcup = arrey.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
         <li class="container-photo">
         <a href="${largeImageURL}"></a>
         <img src="${webformatURL}" alt="${tags}" class="img-photo">
             <ul class="list-text relative">
-                <li class="list-title"><p class="list-text-img">likes: ${likes}</p></li>
-                <li class="list-title"><p class="list-text-img">views: ${views}</p></li>
-                <li class="list-title"><p class="list-text-img">comments: ${comments}</p></li>
-                <li class="list-title"><p class="list-text-img">downloads: ${downloads}</p></li>
+                <li class="list-title"><p class="list-text-img">likes:</br>${likes}</p></li>
+                <li class="list-title"><p class="list-text-img">views:</br>${views}</p></li>
+                <li class="list-title"><p class="list-text-img">comments:</br>${comments}</p></li>
+                <li class="list-title"><p class="list-text-img">downloads:</br>${downloads}</p></li>
             </ul>
         </img>
       </li>
