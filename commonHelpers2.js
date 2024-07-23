4b8aa29faa47193567e60d4fb8fa6e1fc2cf41f3
GenchikS <106490249+GenchikS@users.noreply.github.com>
2024-07-23 12:24:33 +0000

    Deploying to gh-pages from @ GenchikS/Collections-photos@f556801c2c9b5cb65b7481c59a53a2dcfb2b9b00 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,14 +1,14 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const i="44770113-cb4279c01992ac20f8c79d080";function c(e){return fetch(`https://pixabay.com/api/?key=${i}&q=${e}&image_type=photo&orientation=horizontal&safesearch=true&per_page=9`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}const s=document.querySelector(".photo-gallery");function a(e){console.log("arrey",e);const t=e.map(({webformatURL:o,largeImageURL:r,tags:n})=>`
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const p="44770113-cb4279c01992ac20f8c79d080";function u(e){return fetch(`https://pixabay.com/api/?key=${p}&q=${e}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}const m=document.querySelector(".photo-gallery");function g(e){console.log("arrey",e);const t=e.map(({webformatURL:l,largeImageURL:i,tags:s,likes:r,views:n,comments:c,downloads:a})=>`
         <li class="container-photo">
-        <a href="${r}"></a>
-        <img src="${o}" alt="${n}" class="img-photo">
-            <ul class="list-text">
-                <li><p class=""></p></li>
-                <li><p class=""></p></li>
-                <li><p class=""></p></li>
-                <li><p class=""></p></li>
+        <a href="${i}"></a>
+        <img src="${l}" alt="${s}" class="img-photo">
+            <ul class="list-text relative">
+                <li class="list-title"><p class="list-text-img">likes: ${r}</p></li>
+                <li class="list-title"><p class="list-text-img">views: ${n}</p></li>
+                <li class="list-title"><p class="list-text-img">comments: ${c}</p></li>
+                <li class="list-title"><p class="list-text-img">downloads: ${a}</p></li>
             </ul>
         </img>
       </li>
-    `).join("");s.insertAdjacentHTML("beforeend",t)}const p=document.querySelector(".form"),u=document.querySelector(".photo-gallery");p.addEventListener("submit",m);let l="";function m(e){e.preventDefault();const t=e.target;if(l=t.elements.input.value.trim(),console.log("input",l),l===""){console.log("Введіть пошук");return}else c(l).then(o=>{if(Number(o.hits.length)===0)return console.log("Фото не знайдені");u.innerHTML="",a(o.hits)}).catch(o=>console.log("error",o));t.reset()}
+    `).join("");m.insertAdjacentHTML("beforeend",t)}const h=document.querySelector(".form"),f=document.querySelector(".photo-gallery");h.addEventListener("submit",y);let o="";function y(e){e.preventDefault();const t=e.target;if(o=t.elements.input.value.trim(),console.log("input",o),o===""){console.log("Введіть пошук");return}else u(o).then(l=>{if(Number(l.hits.length)===0)return console.log("Фото не знайдені");f.innerHTML="",g(l.hits)}).catch(l=>console.log("error",l));t.reset()}
 //# sourceMappingURL=commonHelpers2.js.map
